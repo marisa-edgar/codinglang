ea77869ee00c81f29dc9523f2774483ed066e070
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-22 11:48:26 -0700

    start javascript

---
+++ codinglang/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#quiz").submit(function(event){
+    event.preventdefault();
+    $("#hide-quiz").hide().
+    const startrek=$("input:radio[name=startrek]:checked").val();
+    const transport=$("input:radio[name=transport]:checked").val();
+    const siblings=$("input:radio[name=sibings]:checked").val();
+  })
+})
