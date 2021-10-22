27435e520b8199ad91db50f05780c58857a6ce77
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-22 13:36:14 -0700

    WIP scripts.js

--- codinglang/js/scripts.js
+++ codinglang/js/scripts.js
@@ -3,7 +3,34 @@ $(document).ready(function(){
     event.preventdefault();
     $("#hide-quiz").hide().
     const startrek=$("input:radio[name=startrek]:checked").val();
+    const food=$("input:radio[name=food]:checked").val();
     const transport=$("input:radio[name=transport]:checked").val();
     const siblings=$("input:radio[name=sibings]:checked").val();
-  })
-})
+    const lofi=("input:radio[name=lofi]:checked").val();
+    let total=0;
+
+    if (startrek === "theog"){
+      total += 1;
+    } else if (startrek === "TNG"){
+      total += 2;
+    }else if (startrek === "voy"{
+      total += 3;
+    }else if (startrek === "Ds9"{
+      total += 4;
+    }else if (startrek === "discovery"{
+      total += 5;
+    };
+    if (food === "hotdog"){
+      total += 1;
+    }else if (food === "spaghetti"){
+      total += 2;
+    }else if (food === "spaghetti"){
+      total += 3;
+    }else if (food === "sandwich"){
+      total += 4;
+    }else if (food === "ramen"){
+      total += 5;
+    };
+
+  });
+});
